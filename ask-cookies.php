c91400353a42fa1515c81d532b08b431b084ae8a
camorobot <41551968+camorobot@users.noreply.github.com>
2019-12-25 23:14:17 +0100

    design cookie page

--- ask-cookies.php
+++ ask-cookies.php
@@ -4,30 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>cookie redirect</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Cookies Boompje5</title>
 </head>
-<style>
-    body{
-        background-color:red;
-    }
-</style>
 <body>
-    <h1>DIT IS EEN TEST WEBSITE!</h1>
-    <?php
-        $homeurl = $_POST['homeurl'];
-        if (isset($_SESSION['tracking']) && $_SESSION['tracking'] == true) {
-            header('location: /');
-        } else{ 
-        }
-    ?>
-    <h1>COOKIES NOG ACCEPTEREN</h1>
-    <p>De site <?php echo $homeurl; ?> maakt gebruik van cookies. u moet deze accepter om door te gaan</p>
-    <form action="assets/php/redirect.php" method="POST">
-        <input type="hidden" name="homeurl" value=" <?php echo $homeurl; ?>">
-        <button type="submit">Ik accepteer de cookies</button>
-    </form>
 
-    
+    <div class="container-background">
+        <div class="inner-container">
+            <img src="assets/img/boompje5-2019.svg" alt="B5 Logo">
+            <div class="container-text" id="text">
+                <p>Op de websites van Boompje5 maken wij gebruik van cookies.
+                Het privacy- en cookiebeleid van Boompje5 geldt voor al onze websites.</p>
+            </div>
+            <div class="container-text container-button">
+                <form action="assets/php/redirect.php" method="POST">
+                    <input type="hidden" name="homeurl" value=" <?php echo $homeurl; ?>">
+                    <button type="submit">Ik accepteer de cookies</button>
+                </form>
+            </div>
+            <div class="container-text" id="link">
+                <p>Ga voor meer informatie naar <a href="https://cookies.boompje5.nl/privacy">cookies.boompje5.nl/privacy </a></p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <?php //$homeurl = $_POST['homeurl']; ?> 
+
 
 </body>
 </html>
